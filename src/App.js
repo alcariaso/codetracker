41d239e925a0ff1cab845efbbe12ebe829c17b5f
Gillian Trethewey <gilliantrethewey100@gmail.com>
2024-02-01 14:01:07 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import './App.scss';
-import HeroSection from './component/HeroSection/HeroSection';
-import NavBar from './components/NavBar/NavBar';
-import Taskform from './components/TaskForm/TaskForm';
-import TaskList from './components/TaskList/TaskList';
-
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8000/tasks`);
+      setTasks(response.data.tasks);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // const toggleTaskCompletion = (id) => {
+  //   setTasks(
+  //     tasks.map((task) =>
+  //       task.id === id ? { ...task, completed: !task.completed } : task
+  //     )
+  //   );
+  // };
   return (
     <>
-      <NavBar/>
-      <HeroSection/>
-      <div className='app__container'>
-      <Taskform/>
-      <TaskList/>
-      </div>
+      <NavBar />
+      <HeroSection />
+      <TaskList tasks={tasks} setTasks={setTasks} />
+
     </>
   );
 }
 
 export default App;
+

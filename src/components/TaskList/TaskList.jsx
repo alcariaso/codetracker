e42f48ddf6eb10de45798ee200d9d39628ea918f
Gillian Trethewey <gilliantrethewey100@gmail.com>
2024-02-01 13:59:13 -0800

    Update TaskList.jsx

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,47 +1,26 @@
-import { useState, useEffect } from "react";
-import axios from 'axios';
+
 import './TaskList.scss';
 
-export default function TaskList() {
-    const { REACT_APP_API_BASE_PATH } = process.env
-    const [tasks, setTasks] = useState([])
-
-    const fetchData = async () => {
-        try {
-            const response = await axios.get(`${REACT_APP_API_BASE_PATH}/tasks`)
-            setTasks(response.data.tasks) // Update the tasks state with the fetched data
-            console.log(tasks)
-        } catch (error) {
-            console.error(error.message)
-        }
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []); 
-
-    const toggleTaskCompletion = (id) => {
-        setTasks(tasks.map(task => 
-            task.id === id ? {...task, completed: !task.completed} : task
-        ))
-    }
+export const TaskList = (tasks, setTasks) => {
+    console.log('tasks:', tasks);
+   
     return (
-        <div className="task-list">
-            <p className="task-list__title">CODE TARCKER LIST</p>
+        <>
+            <p className="task-list">To Do List</p>
             <div className="task-list__container">
-                {tasks.map((task) => (
+                {tasks.tasks.map((task) => (
                     <p key={task.id}>
                         <input 
                             type="checkbox" 
                             name ="completedTask" 
                             id="completedTask" 
                             checked={task.completed}
-                            onChange={() => toggleTaskCompletion(task.id)}
+                            //onChange={() => toggleTaskCompletion(task.id)}
                             />
                     {task.task}
                     </p>
                 ))}
             </div>
-        </div>
+        </>
     );
 }

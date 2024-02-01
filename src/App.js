f96abdacede85d08d1a439cbbb77289ff622d1dd
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-02-01 12:43:16 -0800

    add key for taskForm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import HeroSection from './component/HeroSection/HeroSection';
 import NavBar from './components/NavBar/NavBar';
+import Taskform from './components/TaskForm/TaskForm';
 import TaskList from './components/TaskList/TaskList';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <NavBar/>
       <HeroSection/>
+      <Taskform/>
       <TaskList/>
     </>
   );

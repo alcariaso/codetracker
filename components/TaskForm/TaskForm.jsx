45448a29345f10d99461773f28456afa96e94931
Aldee <aldee.cariaso@gmail.com>
2024-02-01 08:35:41 -0700

    initial taskform

---
+++ components/TaskForm/TaskForm.jsx
@@ -0,0 +1,22 @@
+
+
+
+function TaskForm () {
+
+    return (
+
+        <>
+        <section className="TaskForm">
+            <h1>Code Task List</h1>
+            <form>
+
+
+            </form>
+
+
+        </section>
+
+        </>
+    )
+
+}
